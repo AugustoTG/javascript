be72c08bb9b1414c48b550e92cd467a58005abf9
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-02-02 15:21:44 -0300

    create ex12.js

---
+++ Modulo D/aula11.js
@@ -0,0 +1,9 @@
+ let idade = 66
+
+ if(idade < 16 ){
+    console.log('Não vota!')
+ }else if(idade < 18 || idade > 65){
+        console.log('A pessoa pode votar, se quiser!')
+    }else{
+            console.log('Vota!')
+        }
---
+++ Modulo D/ex012.js
@@ -0,0 +1,15 @@
+let hora = 15
+console.log(`Agora são ${hora} horas!`)
+if (hora >6 && hora < 12){
+    console.log('Periodo da manhã!')
+    console.log('Bom dia!')
+} else if(hora>=12 && hora <= 18){
+    console.log('Periodo da tarde!')
+    console.log('Boa Tarde!')
+} else if( hora > 18 && hora < 24){
+    console.log('Periodo Noturno!')
+    console.log('Boa Noite!')
+}else if (hora >= 0 && hora <= 6){
+    console.log('Madrugada!')
+    console.log('Boa Noite!')
+}
