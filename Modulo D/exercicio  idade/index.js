547d08ae7861e0c3866dbacfdb72a4ca5bf82351
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-02-03 16:01:31 -0300

    update index.js

--- Modulo D/exercicio  idade/index.js
+++ Modulo D/exercicio  idade/index.js
@@ -1,9 +1,20 @@
 function carregar(){
-let data = document.getElementById('ano')
-let sexo = document.getElementById('sex')
+let data = document.getElementById('ano').value
 let result =document.getElementById('resultado')
 let anoatual = new Date()
-let anofinal= anoatual.getFullYear() - data.value
-result.innerHTML = (`Você tem ${anofinal} anos!`)
-result.style.textAlign = ('center')
+let fano = anoatual.getFullYear()
+let anofinal = fano - data
+if(data.length == 0 || data >= fano){
+    window.alert('Erro - Tente novamente!')
+}else{
+    let sexo = document.getElementsByName('tsexo')
+    let genero = ''
+    if(sexo[0].checked){
+        genero = 'Você é um Homem'
+    }else{
+        genero = 'Você é uma Mulher'
+    }
+    result.innerHTML = (`${genero} com ${anofinal} anos!`)
+    result.style.textAlign = ('center')
+}
 }
