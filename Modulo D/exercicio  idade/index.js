b6ebef804d952dc78b0e1989b7dfb1238bc1a282
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-02-03 14:00:18 -0300

    update index.js

--- Modulo D/exercicio  idade/index.js
+++ Modulo D/exercicio  idade/index.js
@@ -1,5 +1,9 @@
+function carregar(){
 let data = document.getElementById('ano')
 let sexo = document.getElementById('sex')
-function(){
-    
+let result =document.getElementById('resultado')
+let anoatual = new Date()
+let anofinal= anoatual.getFullYear() - data.value
+result.innerHTML = (`Você tem ${anofinal} anos!`)
+result.style.textAlign = ('center')
 }
