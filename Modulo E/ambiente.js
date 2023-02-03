fc6a831d866053062372c5033fb8e4d3c7c51d69
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-02-03 19:53:52 -0300

    Update ambiente.js

--- Modulo E/ambiente.js
+++ Modulo E/ambiente.js
@@ -1,5 +1,11 @@
 let  c = 1
-while(c <= 6){
-    console.log('Tudo Ok!')
-    c++
+while(c <= 3){
+    console.log(`Passo ${c}`)
+    c++ // c++ -> c = c+1
 }
+
+
+do{
+    console.log(`Processo com DO WHILE ${c}`)
+    c++ // c++ -> c = c+1
+}while(c <= 5)
