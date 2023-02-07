99f1d52625028b5e9ce550d6d1e94a599f85138c
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-02-07 09:47:44 -0300

    update index.html index.css

--- Modulo F/exercicio  array/index.js
+++ Modulo F/exercicio  array/index.js
@@ -1,15 +1,6 @@
 function carregar(){
-    let entrada = document.getElementById('numero')
-    let tabela = document.getElementById('sel')
-    if(entrada.length == 0){
-        window.alert('Erro - Digite um número!')
-    }else{
-        let n = Number(entrada.value)
-        tabela.innerHTML =''
-        for(let mult =0; mult <= 10; mult++){
-            let item = document.createElement('option')
-            item.text = `${n} X ${mult} = ${n*mult}`
-            tabela.appendChild(item)
-        }
-    }
+    
+}
+function finalizar(){
+    
 }
